ecd5d90a1d014fb75390843a99dabd24fd85068e
Michael <michaelpoland2001@gmail.com>
2022-07-30 13:52:25 +0700

    first commit

---
+++ lib/app/utils/widget/stye/AppColors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppColors{
+  static const primaryBg = Color.fromARGB(255, 96, 187, 35);
+    static const primaryText = Color.fromARGB(255, 0, 0, 0);
+    static const cardBg = Color.fromARGB(255, 247, 240, 240);
+    
+}
